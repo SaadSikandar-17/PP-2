4af12dd6ef718cdfebbbfb5e640861641fffa8dd
SaadSikandar-17 <17238021@gift.edu.pk>
2020-08-15 01:24:45 +0500

    PP-2-6

--- src/Employee/HourlyEmployee.java
+++ src/Employee/HourlyEmployee.java
@@ -1,4 +1,42 @@
 package Employee;
 
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee{
+
+    private double hours;
+    private double hourlyRate;
+    public double getHours() {
+        return hours;
+    }
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public HourlyEmployee(int id, String n, double t, double h, double hr) {
+        super(id,n,t);
+        this.setHours(h);
+        this.setHourlyRate(hr);
+    }
+
+    public double calculateSalary() {
+        double gross, tax, Salary;
+
+        gross= this.getHours() * this.getHourlyRate();
+        tax= gross * this.getTaxRate();
+        Salary= gross - tax;
+        return Salary;
+    }
+
+    public String toString() {
+        String x;
+        String h=Double.toString(this.getHours());
+        String hr =Double.toString(this.getHourlyRate());
+        x= super.toString()+" "+h+" "+hr;
+        return x;
+    }
 }
