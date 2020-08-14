e4dc5bfcdc69146b6832416654fe78d3f57ac344
SaadSikandar-17 <17238021@gift.edu.pk>
2020-08-15 01:08:14 +0500

    PP-2-2

--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -1,4 +1,46 @@
 package Employee;
 
 public class Employee {
+    private int empId;
+    private String Name;
+    private double taxRate;
+
+    public Employee() {
+        this.setEmpId(0);
+        this.setName("");
+        this.setTaxRate(0.0);
+    }
+    public Employee(int id, String n, double t) {
+        this.setEmpId(id);
+        this.setName(n);
+        this.setTaxRate(t);
+    }
+    public double calculateSalary() {
+
+        return 0.0;
+    }
+
+    public String toString() {
+        return this.getEmpId()+ " " +this.getName()+" "+this.getTaxRate();
+    }
+    public int getEmpId() {
+        return empId;
+    }
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public double getTaxRate() {
+        return taxRate;
+    }
+    public void setTaxRate(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+
 }
