0cf4ec5d60f326b7b4bff93ad35f8934e34065e4
SaadSikandar-17 <17238021@gift.edu.pk>
2020-08-15 01:15:10 +0500

    PP-2-4

--- src/Employee/SalariedEmployee.java
+++ src/Employee/SalariedEmployee.java
@@ -1,4 +1,30 @@
 package Employee;
 
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee {
+
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public SalariedEmployee(int id, String n, double t, double s) {
+        super(id,n,t);
+        this.setSalary(s);
+    }
+
+    public double calculateSalary () {
+        double Salary;
+        Salary= this.getSalary() - (this.getSalary() * this.getTaxRate());
+        return Salary;
+    }
+    public String toString() {
+        String x;
+        x= super.toString()+ " "+this.getSalary();
+        return x;
+    }
 }
